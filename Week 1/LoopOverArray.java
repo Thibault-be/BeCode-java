1a3061ec63119f2d503e40403327b066be38ae8c
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-02 12:27:04 +0100

    Add dynamic upper limit to  LoopOverArray

--- Week 1/LoopOverArray.java
+++ Week 1/LoopOverArray.java
@@ -1,14 +1,21 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class LoopOverArray {
     public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
         int[] intArray = new int[20];
         for(int i = 0; i < 20 ; i++){
             intArray[i]= GenerateRandomInteger();
         }
 
+        System.out.print("What is your upper limit? ");
+        int upperLimitForPrinting = Integer.parseInt(scanner.nextLine());
+
         for (int i = 0; i < intArray.length; i++){
-            if (intArray[i] < 10){
+            if (intArray[i] <= upperLimitForPrinting){
                 System.out.println(intArray[i]);
             }
         }
