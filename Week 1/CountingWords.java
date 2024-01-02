3c93f870906e8f4d9abd95872b0eb68883d681db
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-02 16:14:48 +0100

    CountingWords - special characters and casing not accounted for

--- Week 1/CountingWords.java
+++ Week 1/CountingWords.java
@@ -1,7 +1,7 @@
 import java.nio.file.Path;
 import java.util.Scanner;
 import java.nio.file.Paths;
-
+import java.util.ArrayList;
 
 public class CountingWords{
 
@@ -9,17 +9,65 @@ public class CountingWords{
 
         Path path = Paths.get("/home/tbo/repos/BeCode-java/Week 1/assets/week-1/text.txt");
 
+        ArrayList<String> wordsList = new ArrayList<>();
+        ArrayList<String[]> countList = new ArrayList<>();
+
+        String[] appearsMost = new String[]{};
+        int highestCount = 0;
+        int lineCount = 0;
 
         try (Scanner scanner = new Scanner(path)){
             while (scanner.hasNextLine()){
+                lineCount++;
                 String line = scanner.nextLine();
-                System.out.println(line);
+
+                //break up each line into parts
+                String[] parts = line.split(" ");
+
+                //go over each piece of parts
+                for (String piece : parts){
+
+                    if (!wordsList.contains(piece)){
+                        wordsList.add(piece);
+
+                        String[] firstOccurrence = {piece, "1"};
+                        countList.add(firstOccurrence);
+                        continue;
+                    }
+
+                    for (int i = 0; i < countList.size(); i++){
+                        if (countList.get(i)[0].equals(piece)){
+                            int currentCount = Integer.parseInt(countList.get(i)[1]);
+                            currentCount++;
+                            countList.get(i)[1] = String.valueOf(currentCount);
+                        }
+                    }
+                }
+
+                for (String[] item : countList){
+                    int count = Integer.parseInt(item[1]);
+
+                    if (count > highestCount){
+                        highestCount = count;
+                        appearsMost = item;
+                    }
+
+                }
             }
         } catch (Exception e){
             System.out.println(e.getMessage());
         }
 
+        System.out.println("The word that appears the most is " + appearsMost[0] + " and is printed " + appearsMost[1] + " times." );
 
+        for (String[] item : countList){
+            for (String piece : item){
+                System.out.print(piece);
+                System.out.print(" ");
+            }
+            System.out.println();
 
+        }
+        System.out.println(lineCount);
     }
 }
