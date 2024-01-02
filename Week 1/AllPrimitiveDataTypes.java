b2d142f7cba7771176730100d86acefe2af5f9e3
Thibault-be <thibault.dhooghe@gmail.com>
2024-01-02 10:26:41 +0100

    Week 1 - Primitive Data Types

---
+++ Week 1/AllPrimitiveDataTypes.java
@@ -0,0 +1,26 @@
+public class AllPrimitiveDataTypes {
+
+  public static void main(String[] args){
+
+    // all types: byte, char, int, float, long, double, boolean, short
+
+    byte  a = -126;
+    char b = '+';
+    int c = 5;
+    float d = 5.0f;
+    long e = 256987L;
+    double f = 45.5468;
+    boolean g = true;
+    short h = 26487;
+
+    System.out.println("byte a " + a);
+    System.out.println("char b " + b);
+    System.out.println("int c " + c);
+    System.out.println("float d " + d);
+    System.out.println("long e " + e);
+    System.out.println("double f" + f);
+    System.out.println("boolean g "  + g);
+    System.out.println("short h " + h);
+    
+  }
+}
